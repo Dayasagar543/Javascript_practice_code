24cc2de9c16acda56284932993aa6631570b96c7
Dayasagar543 <dayasagar.543@gmail.com>
2022-11-27 20:24:08 +0530

    modified the area of the circle in the  file

--- file7.js
+++ file7.js
@@ -4,5 +4,5 @@ console.log(pie);
 // if we try to change this would give errors
 // pie =2.12
 // you can persform operations using the const value but cannot change the value once declaring using const.
-let area_circle = pie * 2 ** 2
+let area_circle = pie * (2 ** 2)
 console.log("area_circle is :"+ " "+ area_circle );

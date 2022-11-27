0ca1fe9ab57cbdbdc56a28b50213cac6d5bca6d0
Dayasagar543 <dayasagar.543@gmail.com>
2022-11-27 16:01:47 +0530

    commiting the files that are empty and also loaded with code thes might get updated soon file from 7-18

---
+++ file7.js
@@ -0,0 +1,8 @@
+//declaring constants eg :pie 
+const pie = 3.14;
+console.log(pie);
+// if we try to change this would give errors
+// pie =2.12
+// you can persform operations using the const value but cannot change the value once declaring using const.
+let area_circle = pie * 2 ** 2
+console.log("area_circle is :"+ " "+ area_circle );
